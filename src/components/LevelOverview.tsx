f03c795fba846c4cdc285174bd86f1811f02541a
Anthony Chung <75187566+anthonytchung@users.noreply.github.com>
2025-04-05 23:16:34 -0500

    Level Overview accessibility

--- src/components/LevelOverview.tsx
+++ src/components/LevelOverview.tsx
@@ -1,6 +1,8 @@
 // /components/LevelOverview.tsx
 "use client";
+"use client";
 import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 interface LevelOverviewProps {
   title: string;
@@ -10,17 +12,20 @@ interface LevelOverviewProps {
 
 export default function LevelOverview({ title, description, startRoute }: LevelOverviewProps) {
   const router = useRouter();
+  const t = useTranslations("levelOverview");
 
   return (
-    <div className="min-h-screen p-10 bg-white flex flex-col items-center justify-center font-mono">
-      <h1 className="text-4xl font-bold mb-4">{title}</h1>
-      <p className="text-lg text-gray-700 mb-10 text-center max-w-xl">{description}</p>
+    <main role="main" className=" min-h-screen p-10 bg-white flex flex-col items-center justify-center font-mono">
+      <h1 tabIndex={0} className="text-4xl font-bold mb-4 text-black">{title}</h1>
+      <p tabIndex={0} className="text-lg text-gray-700 mb-10 text-center max-w-xl">{description}</p>
       <button
+        tabIndex={0}
         onClick={() => router.push(startRoute)}
         className="cursor-pointer px-6 py-3 bg-green-800 text-white rounded-md hover:bg-green-700 transition"
+        aria-label={t("startAriaLabel")}
       >
-        Start
+        {t("start")}
       </button>
-    </div>
+    </main>
   );
 }
