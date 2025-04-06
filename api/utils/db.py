90142427f4e7f984c9c8a8c4c51968f5a8b74526
elainecui9 <elaine.cui@gmail.com>
2025-04-06 13:19:13 -0500

    fix key

--- api/utils/db.py
+++ api/utils/db.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 session = boto3.Session(
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
     region_name=os.getenv('AWS_REGION')
 )
 dynamodb = session.resource('dynamodb')
