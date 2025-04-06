4d83713cff71215e0e52e2ddc3d224c3e9ca0e7b
heochristopher <71462915+heochristopher@users.noreply.github.com>
2025-04-05 19:36:49 -0500

    .

--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from routes import auth, dashboard, levels, generateContent
 
+
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(dashboard.router)
 app.include_router(levels.router)
+<<<<<<< HEAD
 app.include_router(generateContent.router)
+=======
+app.include_router(language.router)
+>>>>>>> 47beae3 (.)
