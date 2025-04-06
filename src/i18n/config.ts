49f21628180f66ead0aea9c59188b94a7ef027b8
Anthony Chung <75187566+anthonytchung@users.noreply.github.com>
2025-04-06 11:48:12 -0500

    Update config.ts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,7 +1,7 @@
 import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 
-export const locales = ['en', 'ko'] as const;
+export const locales = ['en', 'ko', 'de'] as const;
 export type Locale = (typeof locales)[number];
 
 export default getRequestConfig(async ({locale}) => {
